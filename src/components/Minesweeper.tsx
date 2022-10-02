f47b910f6253f482c5c3780e85e297b3cff0a873
Natasha Mann <natasha.s.mann@gmail.com>
2022-10-02 15:38:37 +0100

    enhanced game logic

--- src/components/Minesweeper.tsx
+++ src/components/Minesweeper.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { minesweeper } from "../utils";
+import { getSurroundingSpaceIndices, minesweeper } from "../utils";
 import styles from "./minesweeper.module.css";
 import bombImg from "../assets/bomb.png";
+import flagImg from "../assets/red-flag.png";
 
 interface MinesweeperProps {
   gameArray: string[];
@@ -12,27 +13,105 @@ export const Minesweeper = ({ gameArray, setGameOver }: MinesweeperProps) => {
   const [bomb, setBomb] = useState(false);
   const gameResult = minesweeper(gameArray);
 
+  const setBombSpace = (space: Element) => {
+    space.setAttribute("class", `${styles.selected} ${styles.bomb}`);
+    setBomb(true);
+
+    const image = document.createElement("img");
+    image.setAttribute("src", bombImg);
+    image.setAttribute("class", styles.bombImg);
+    space.appendChild(image);
+    setTimeout(() => {
+      setGameOver(true);
+    }, 3000);
+  };
+
+  const setSurroundingSpaces = (space: number, result: string) => {
+    const surroundingSpace = document.querySelector(`[data-id="${space}"]`)!;
+
+    if (result === "X") {
+      setBombSpace(surroundingSpace);
+      return;
+    }
+
+    surroundingSpace.setAttribute("class", `${styles.selected} ${styles.safe}`);
+
+    if (result === "0") {
+      const surroundingIndices = getSurroundingSpaceIndices(
+        space,
+        gameArray.length
+      );
+
+      surroundingIndices.forEach((e) => {
+        const surroundingSpace = document.querySelector(`[data-id="${e}"]`)!;
+
+        if (
+          !surroundingSpace.classList.contains(`${styles.selected}`) &&
+          gameResult[e] !== "X"
+        ) {
+          setSurroundingSpaces(e, gameResult[e]);
+        }
+      });
+    } else {
+      surroundingSpace.textContent = gameResult[space];
+    }
+  };
+
   const displayResult = (e: React.MouseEvent) => {
     const selectedSpace = e.currentTarget;
+
+    if (selectedSpace.classList.contains(`${styles.flagged}`)) {
+      return;
+    }
+
     const i = e.currentTarget.getAttribute("data-id");
     const currentIndex = Number(i);
 
     if (gameResult[currentIndex] === "X") {
-      setBomb(true);
-      selectedSpace.setAttribute("class", `${styles.selected} ${styles.bomb}`);
+      setBombSpace(selectedSpace);
+      return;
+    }
 
-      const image = document.createElement("img");
-      image.setAttribute("src", bombImg);
-      image.setAttribute("class", styles.image);
-      selectedSpace.appendChild(image);
-      setTimeout(() => {
-        setGameOver(true);
-      }, 3000);
-    } else if (gameResult[currentIndex] === "0") {
-      // set to selected style, but also any other adjacent squares which are 0 should be cleared
+    if (gameResult[currentIndex] === "0") {
+      selectedSpace.setAttribute("class", `${styles.selected} ${styles.safe}`);
+
+      const surroundingIndices = getSurroundingSpaceIndices(
+        currentIndex,
+        gameArray.length
+      );
+
+      const surroundingSpaces = surroundingIndices.map((e) => {
+        return { result: gameResult[e], surroundingIndex: e };
+      });
+
+      surroundingSpaces.forEach(({ result, surroundingIndex }) => {
+        if (result === "0") {
+          setSurroundingSpaces(surroundingIndex, result);
+        }
+      });
     } else {
       selectedSpace.setAttribute("class", `${styles.selected} ${styles.safe}`);
       selectedSpace.textContent = gameResult[currentIndex];
+    }
+  };
+
+  const toggleFlag = (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log("FLAGGED");
+    const selectedSpace = e.currentTarget;
+
+    if (selectedSpace.hasChildNodes()) {
+      selectedSpace.removeChild(selectedSpace.getElementsByTagName("img")[0]);
+      selectedSpace.setAttribute("class", `${styles.box}`);
+    } else {
+      selectedSpace.setAttribute(
+        "class",
+        `${styles.selected} ${styles.flagged}`
+      );
+      const image = document.createElement("img");
+      image.setAttribute("src", flagImg);
+      image.setAttribute("class", styles.flagImg);
+      selectedSpace.appendChild(image);
     }
   };
 
@@ -43,6 +122,7 @@ export const Minesweeper = ({ gameArray, setGameOver }: MinesweeperProps) => {
           <div
             className={styles.box}
             onClick={!bomb ? displayResult : undefined}
+            onContextMenu={toggleFlag}
             data-id={index}
             key={index}
           ></div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,37 +1,52 @@
+export const getSurroundingSpaceIndices = (
+  index: number,
+  arrLength: number
+): number[] => {
+  let surroundingIndices = [
+    index - 6,
+    index - 5,
+    index - 4,
+    index + 1,
+    index + 6,
+    index + 5,
+    index + 4,
+    index - 1,
+  ];
+
+  const squareRoot = Math.sqrt(arrLength);
+
+  if ((index + 1) % squareRoot === 0) {
+    surroundingIndices = surroundingIndices.filter(
+      (e) => e !== index + 6 && e !== index - 4 && e !== index + 1
+    );
+  }
+
+  if (index % squareRoot === 0 || index === 0) {
+    surroundingIndices = surroundingIndices.filter(
+      (e) => e !== index - 6 && e !== index + 4 && e !== index - 1
+    );
+  }
+
+  surroundingIndices = surroundingIndices.filter(
+    (e) => e >= 0 && e < arrLength
+  );
+
+  return surroundingIndices;
+};
+
 export const minesweeper = (arr: string[]) => {
   return arr.map((space: string, index: number, array: string[]): string => {
     if (space === "X") {
       return "X";
     }
 
-    const surroundingSpaces = {
-      spaceRight: array[index + 1] === "X",
-      spaceLeft: array[index - 1] === "X",
-      spaceAbove: array[index - 5] === "X",
-      spaceBelow: array[index + 5] === "X",
-      topLeft: array[index - 6] === "X",
-      topRight: array[index - 4] === "X",
-      bottomLeft: array[index + 4] === "X",
-      bottomRight: array[index + 6] === "X",
-    };
-
     let bombs = 0;
 
-    Object.values(surroundingSpaces).forEach((e) => {
-      if (e === true) bombs += 1;
+    const surroundingIndices = getSurroundingSpaceIndices(index, array.length);
+
+    surroundingIndices.forEach((i) => {
+      if (array[i] === "X") bombs += 1;
     });
-
-    if ((index + 1) % 5 === 0) {
-      if (surroundingSpaces.spaceRight) bombs -= 1;
-      if (surroundingSpaces.topRight) bombs -= 1;
-      if (surroundingSpaces.bottomRight) bombs -= 1;
-    }
-
-    if (index % 5 === 0) {
-      if (surroundingSpaces.spaceLeft) bombs -= 1;
-      if (surroundingSpaces.bottomLeft) bombs -= 1;
-      if (surroundingSpaces.topLeft) bombs -= 1;
-    }
 
     return bombs.toString();
   });
